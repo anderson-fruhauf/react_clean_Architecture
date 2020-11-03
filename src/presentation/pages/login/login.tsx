4f8335cf0c59e15abdf73b446ae093d345f8e4b6
Anderson Augusto Leichtweis Von Fruhauf <anderson.fruhauf@insidesistemas.com.br>
2020-11-03 16:44:58 -0300

    feat: implement login view

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Styles from './login-styles.scss'
+import Spinner from '@/presentation/components/spinner/spinner'
+import LoginHeader from '@/presentation/components/login-header/login-header'
+import Footer from '@/presentation/components/footer/footer'
+
+const Login: React.FC = () => {
+  return (
+    <div className={Styles.login}>
+
+      <LoginHeader />
+
+      <form className={Styles.form}>
+        <h2>Login</h2>
+        <div className={Styles.inputWrap}>
+          <input type="email" name='email' placeholder='Digite seu e-mail' />
+          <span className={Styles.status}>⛔</span>
+        </div>
+        <div className={Styles.inputWrap}>
+          <input type="password" name='password' placeholder='Digite seu e-mail' />
+          <span className={Styles.status}>⛔</span>
+        </div>
+        <button className={Styles.submit} type="submit">Entrar</button>
+        <span className={Styles.link}>Criar conta</span>
+
+        <div className={Styles.errorWrap}>
+          <Spinner className={Styles.spinner} />
+          <span className={Styles.error}>error</span>
+        </div>
+      </form>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Login
