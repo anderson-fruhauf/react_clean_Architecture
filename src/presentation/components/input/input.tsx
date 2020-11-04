34f0fdc7d5cfbdccb1240175f0cd63169a7b8691
Anderson Augusto Leichtweis Von Fruhauf <anderson.fruhauf@insidesistemas.com.br>
2020-11-04 11:00:37 -0300

    feat: ensure login validations

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,8 +5,16 @@ import Context from '@/presentation/context/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context)
-  const error = errorState[props.name]
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
+  }
+
   const getStatus = (): string => {
     return '⛔'
   }
@@ -15,7 +23,7 @@ const Input: React.FC<Props> = (props: Props) => {
   }
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} />
+      <input data-testid={props.name} {...props} onChange={handleChange} />
       <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
 import Login from './login'
+import { Validation } from '@/presentation/protocols/validation'
+
+type SutTypes = {
+  sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy} />)
+  return {
+    sut,
+    validationSpy
+  }
+}
+
+class ValidationSpy implements Validation {
+  errorMesage: string
+  input: object
+  validate(input: object): string {
+    this.input = input
+    return this.errorMesage
+  }
+}
 
 describe('Login Component', () => {
+  afterEach(cleanup)
+
   test('Should start with initial state', () => {
-    const { getByTestId } = render(<Login />)
+    const { getByTestId } = makeSut().sut
     const errorWrap = getByTestId('error-wrap')
     const sumbitButton = getByTestId('submit') as HTMLButtonElement
     const emailStatus = getByTestId('email-status')
@@ -16,5 +42,23 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(emailStatus.textContent).toBe('⛔')
     expect(passwordStatus.textContent).toBe('⛔')
+  })
+
+  test('Should call email Validation with correct value', () => {
+    const { sut, validationSpy } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validationSpy.input).toEqual({
+      email: 'any_email'
+    })
+  })
+
+  test('Should call password Validation with correct value', () => {
+    const { sut, validationSpy } = makeSut()
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    })
   })
 })
