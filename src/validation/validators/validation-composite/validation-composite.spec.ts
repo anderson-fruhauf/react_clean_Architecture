e8a17c59c7e2aa6e0c188857e65a1d333d19ea4e
Anderson Augusto Leichtweis Von Fruhauf <anderson.fruhauf@insidesistemas.com.br>
2020-11-05 15:02:20 -0300

    feat: ensure validationComposite returns falsy if falidations dont fails

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -2,14 +2,35 @@ import faker from 'faker'
 import { ValidationComposite } from './validation-composite'
 import { FieldValidationSpy } from '@/validation/test/FieldValidationSpy'
 
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidatioSpys: FieldValidationSpy[]
+  fieldName: string
+}
+
+const makeSut = (fieldName: string = faker.database.column()): SutTypes => {
+  const fieldValidatioSpys = [
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
+  ]
+  const sut = new ValidationComposite(fieldValidatioSpys)
+  return { sut, fieldValidatioSpys, fieldName }
+}
+
 describe('ValidationComposite', () => {
   test('should return error if any validation fails', () => {
-    const validatorSpy = new FieldValidationSpy('fieldName')
-    const validatorSpy2 = new FieldValidationSpy('fieldName')
-    const sut = new ValidationComposite([validatorSpy, validatorSpy2])
-    validatorSpy.validate.mockReturnValue(new Error('any_message'))
-    const error = sut.validate('fieldName', 'any_value')
-    expect(error).toBe('any_message')
-    expect(validatorSpy2.validate).not.toHaveBeenCalled()
+    const { sut, fieldValidatioSpys, fieldName } = makeSut()
+    const errorMessage = faker.random.words(4)
+    fieldValidatioSpys[0].validate.mockReturnValue(new Error(errorMessage))
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBe(errorMessage)
+    expect(fieldValidatioSpys[1].validate).not.toHaveBeenCalled()
+  })
+
+  test('should return falsy if validation not fails', () => {
+    const { sut, fieldValidatioSpys, fieldName } = makeSut()
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBeFalsy()
+    expect(fieldValidatioSpys[1].validate).toHaveBeenCalled()
   })
 })
