46680602f56e7bd1a2bcfb747890f36b97266681
Anderson Augusto Leichtweis Von Fruhauf <anderson.fruhauf@insidesistemas.com.br>
2020-11-05 13:55:50 -0300

    fix: ensure email validator return falsy if email is empty

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -6,6 +6,6 @@ export class EmailValidation implements FieldFalidation {
 
   validate(value: string): Error {
     const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    return emailRegex.test(value) ? null : new InvalidFieldErro()
+    return (!value || emailRegex.test(value)) ? null : new InvalidFieldErro()
   }
 }
