fef6cc0719ce7b1671852abf635efab79ad4ca7c
Anderson Augusto Leichtweis Von Fruhauf <anderson.fruhauf@insidesistemas.com.br>
2020-11-07 10:16:33 -0300

    feat: add comparaValidationField

---
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -0,0 +1,23 @@
+import { InvalidFieldErro } from '@/validation/errors'
+import faker from 'faker'
+import { CompareFieldValidation } from './compare-fields-validation'
+
+const makeSut = (
+  valueToCompare: string = faker.random.word()
+): CompareFieldValidation =>
+  new CompareFieldValidation(faker.database.column(), valueToCompare)
+
+describe('EmailValidation', () => {
+  test('Should return error if compare is invalid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.random.word())
+    expect(error).toEqual(new InvalidFieldErro())
+  })
+
+  test('Should return falsy if compare is not invalid', () => {
+    const valueToCompare = faker.random.word()
+    const sut = makeSut(valueToCompare)
+    const error = sut.validate(valueToCompare)
+    expect(error).toBeFalsy()
+  })
+})
---
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -0,0 +1,13 @@
+import { InvalidFieldErro } from '@/validation/errors'
+import { FieldValidation } from '@/validation/protocols'
+
+export class CompareFieldValidation implements FieldValidation {
+  constructor (
+    readonly field: string,
+    private readonly valueToCompare: string
+  ) {}
+
+  validate (value: string): Error {
+    return value !== this.valueToCompare ? new InvalidFieldErro() : null
+  }
+}
