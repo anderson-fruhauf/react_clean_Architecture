41ba7b569616569f2a417367e290f84328dc7203
Anderson Augusto Leichtweis Von Fruhauf <anderson.fruhauf@insidesistemas.com.br>
2020-11-10 13:52:37 -0300

    feat: create survey base page

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import SurveiList from './../../pages/survey-list/survei-list'
 
 type Factory = {
   makeLogin: React.FC
@@ -12,6 +13,7 @@ const Router: React.FC<Factory> = (factory: Factory) => {
       <Switch>
         <Route path="/login" exact component={factory.makeLogin} />
         <Route path="/signup" exact component={factory.makeSignUp} />
+        <Route path="/" exact component={SurveiList} />
       </Switch>
     </BrowserRouter>
   )
---
+++ src/presentation/pages/survey-list/survei-list.tsx
@@ -0,0 +1,44 @@
+import { Footer, Logo } from '@/presentation/components'
+import React from 'react'
+import Styles from './survei-list-styles.scss'
+
+const SurveiList: React.FC = () => {
+  return (
+    <div className={Styles.surveiListWrap}>
+      <header className={Styles.headerWrap}>
+        <div className={Styles.headerContent}>
+          <Logo />
+          <div className={Styles.logoutWrap}>
+            <span>Rodrigo</span>
+            <a href="">sair</a>
+          </div>
+        </div>
+      </header>
+      <div className={Styles.contentWrap}>
+        <h2>Enquetes</h2>
+        <ul>
+          <li>
+            <div className={Styles.surveyContent}>
+              <div className={[Styles.iconWrap, Styles.green].join(' ')}>
+                <img
+                  className={Styles.icon}
+                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA0klEQVQ4EWNgIAH8//+/AYhLSNCCWynUMCD1/zcQG+BWSYQMkmEgA0Egjght2JUANYO8iQ4MsasmIAo0BZthP4DirAS0YkrjMAzk0tOYqgmIADUVgnTiADPxakfStAWmECj2DkmcWOYjoEJPRpBqmEGMQABiI4vB5IikH1PbQAYmIm0mVtlLahu4nJpe/gf0hho1XbgVGKd3qWngRFBA4/LyX6AcKZZdBbpOB2QgLk1nQJIkgElwtaBEDAXIOUULKHYSiP/CJHHQX4Hic4CYBWYgADx8PyqFiuhJAAAAAElFTkSuQmCC"
+                />
+              </div>
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>02</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual o framework web favorito</p>
+            </div>
+            <footer>Ver Resultado</footer>
+          </li>
+        </ul>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default SurveiList
