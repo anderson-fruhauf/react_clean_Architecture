9134308925f1693b2b41545505e7673631424d70
Anderson Augusto Leichtweis Von Fruhauf <anderson.fruhauf@insidesistemas.com.br>
2020-11-12 10:47:10 -0300

    feat: ensure SurveyListComponent render with correct values

--- src/presentation/pages/survey-list/survei-list.tsx
+++ src/presentation/pages/survey-list/survei-list.tsx
@@ -1,16 +1,23 @@
 import { Footer, Header } from '@/presentation/components'
-import React, { useEffect } from 'react'
-import { SurveyItemEmpty } from './components'
+import React, { useEffect, useState } from 'react'
+import { SurveyItem, SurveyItemEmpty } from './components'
 import Styles from './survei-list-styles.scss'
 import { LoadSurveyList } from '@/domain/usecases'
+import { SurveyModel } from '@/domain/models'
 
 type Props = {
   loadSurveyList: LoadSurveyList
 }
 
 const SurveiList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  const [state, setState] = useState({
+    surveys: [] as SurveyModel[]
+  })
+
   useEffect(() => {
-    loadSurveyList.loadAll()
+    loadSurveyList.loadAll().then((surveys) => {
+      setState({ ...state, surveys })
+    })
   }, [])
 
   return (
@@ -19,7 +26,11 @@ const SurveiList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul data-testid="survey-list">
-          <SurveyItemEmpty />
+          {state.surveys.length ? (
+            state.surveys.map((survey: SurveyModel) => <SurveyItem survey={survey} />)
+          ) : (
+            <SurveyItemEmpty />
+          )}
         </ul>
       </div>
       <Footer />
--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { SurveiList } from '@/presentation/pages'
 import { LoadSurveyList } from '@/domain/usecases'
+import { mockSurveyList } from './../../../domain/test/mock-survey'
 
 class LoadSurveyListSpy implements LoadSurveyList {
   loadAll = jest.fn()
@@ -13,18 +14,29 @@ type SutType = {
 
 const makeSut = (): SutType => {
   const loadSurveyListSpy = new LoadSurveyListSpy()
+  loadSurveyListSpy.loadAll.mockResolvedValue(mockSurveyList(4))
   render(<SurveiList loadSurveyList={loadSurveyListSpy} />)
   return { loadSurveyListSpy }
 }
 
 describe('SurveyList Component', () => {
-  test('Shoul present 4 empty items on start', () => {
+  test('Shoul present 4 empty items on start', async () => {
     makeSut()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+    await waitFor(() => surveyList)
   })
-  test('Shoul call LoadSurveyList', () => {
+
+  test('Shoul call LoadSurveyList', async () => {
     const { loadSurveyListSpy } = makeSut()
     expect(loadSurveyListSpy.loadAll).toHaveBeenCalled()
+    await waitFor(() => screen.getByText('Enquetes'))
+  })
+
+  test('Should render SurveyItem on success', async () => {
+    makeSut()
+    const surveyList = screen.getByTestId('survey-list')
+    await waitFor(() => surveyList)
+    expect(surveyList.querySelectorAll('li.surveyItemWrap')).toHaveLength(4)
   })
 })
